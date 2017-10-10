b0d3ebf50fba794b634d4e9598c3607ed8029344
Andrew Wenman <goullet86@yahoo.com>
2017-10-10 21:52:34 +0000

    Fixed ENV error

--- config/environment.rb
+++ config/environment.rb
@@ -5,10 +5,10 @@ require_relative 'application'
 Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
-  :port           => Env['MAILGUN_SMTP_PORT'],
-  :address        => Env['MAILGUN_SMTP_SERVER'],
-  :user_name      => Env['MAILGUN_SMTP_LOGIN'],
-  :password       => Env['MAILGUN_SMTP_PASSWORD'],
+  :port           => ENV['MAILGUN_SMTP_PORT'],
+  :address        => ENV['MAILGUN_SMTP_SERVER'],
+  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
   :domain         => 'guarded-anchorage-43966.herokuapp.com',
   :authentication => :plain
 }
